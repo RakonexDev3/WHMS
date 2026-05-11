73bbde55cd0ca711267a5ab1949a72f7a66cba76
Aravind R <aravindrajeshchry@gmail.com>
2026-05-11 14:56:08 +0530

    feat(Storage Bin): Update rack occupancy

--- warehouse_management/warehouse_management/doctype/storage_bin/storage_bin.py
+++ warehouse_management/warehouse_management/doctype/storage_bin/storage_bin.py
@@ -9,6 +9,38 @@ class StorageBin(Document):
 
     def validate(self):
         self.validate_capacity()
+        
+        
+    def after_insert(self):
+            self.update_rack_occupancy()
+            
+            
+    def on_update(self):
+            self.update_rack_occupancy()
+            
+            
+    def on_trash(self):
+            self.update_rack_occupancy()
+            
+            
+    def update_rack_occupancy(self):
+        
+        occupied = frappe.db.sql("""
+			SELECT COALESCE(SUM(cells_occupied), 0)
+			FROM `tabStorage Bin`
+			WHERE rack = %s
+		""", self.rack)[0][0]
+        
+        rack = frappe.get_doc("Rack", self.rack)
+        rack.occupied_cells = occupied or 0
+        rack.available_cells = (
+			rack.total_cells - (occupied or 0)
+		)
+        if rack.available_cells <= 0:
+            rack.status = "Full"
+        else:
+            rack.status = "Active"
+        rack.save(ignore_permissions=True)
 
     def validate_capacity(self):
 
@@ -19,12 +51,11 @@ class StorageBin(Document):
             FROM `tabStorage Bin`
             WHERE rack = %s
             AND name != %s
-            AND docstatus < 2
-        """, (self.rack, self.name))[0][0]
+        """, (self.rack, self.name))[0][0] or 0
 
-        available = rack.total_cells - occupied
+        available = rack.total_cells - (occupied or 0)
 
         if self.cells_occupied > available:
-            frappe.throw(_(
+            frappe.throw(
                 f"Only {available} cells available in Rack {self.rack}"
-            ))
+            )
