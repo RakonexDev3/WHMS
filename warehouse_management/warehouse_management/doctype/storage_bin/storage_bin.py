8d0ad92c458e7a31707f29e0ec9fb5e8ed1b9a7b
Aravind R <aravindrajeshchry@gmail.com>
2026-05-11 14:18:11 +0530

    feat(Storage Bin): Validate cell capacity of linked rack

--- warehouse_management/warehouse_management/doctype/storage_bin/storage_bin.py
+++ warehouse_management/warehouse_management/doctype/storage_bin/storage_bin.py
@@ -1,9 +1,30 @@
 # Copyright (c) 2026, Rakonex and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+# from frappe import _
 
 class StorageBin(Document):
-	pass
+
+    def validate(self):
+        self.validate_capacity()
+
+    def validate_capacity(self):
+
+        rack = frappe.get_doc("Rack", self.rack)
+
+        occupied = frappe.db.sql("""
+            SELECT COALESCE(SUM(cells_occupied), 0)
+            FROM `tabStorage Bin`
+            WHERE rack = %s
+            AND name != %s
+            AND docstatus < 2
+        """, (self.rack, self.name))[0][0]
+
+        available = rack.total_cells - occupied
+
+        if self.cells_occupied > available:
+            frappe.throw(_(
+                f"Only {available} cells available in Rack {self.rack}"
+            ))
