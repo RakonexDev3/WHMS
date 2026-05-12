026865a0dea478ee19c80305a95f347f1e819178
Aravind R <aravindrajeshchry@gmail.com>
2026-05-12 12:29:40 +0530

    feat(Bin Assignment): Update storage bin status on assignment

--- warehouse_management/warehouse_management/doctype/bin_assignment/bin_assignment.py
+++ warehouse_management/warehouse_management/doctype/bin_assignment/bin_assignment.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2026, Rakonex and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class BinAssignment(Document):
-	pass
+	def after_insert(self):
+		"""Set Storage Bin status to Occupied when Bin Assignment is created"""
+		if self.bin:
+			storage_bin = frappe.get_doc("Storage Bin", self.bin)
+			storage_bin.status = "Occupied"
+			storage_bin.save(ignore_permissions=True)
+
+	def after_delete(self):
+		"""Set Storage Bin status to Empty when Bin Assignment is deleted"""
+		if self.bin:
+			storage_bin = frappe.get_doc("Storage Bin", self.bin)
+			storage_bin.status = "Empty"
+			storage_bin.save(ignore_permissions=True)
