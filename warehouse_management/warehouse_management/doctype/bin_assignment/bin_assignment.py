b482324190211563aef18d37f7a6e916d94c3f2b
tinumaria <tinumariadavis@gmail.com>
2026-07-28 16:59:56 +0530

    feat(Bin Assignment): handle storage bin balance

--- warehouse_management/warehouse_management/doctype/bin_assignment/bin_assignment.py
+++ warehouse_management/warehouse_management/doctype/bin_assignment/bin_assignment.py
@@ -3,21 +3,48 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class BinAssignment(Document):
 	def after_insert(self):
 		"""Set Storage Bin status to Occupied when Bin Assignment is created"""
-		if self.bin:
-			storage_bin = frappe.get_doc("Storage Bin", self.bin)
+		if not self.bin:
+			return
+
+		storage_bin = frappe.get_doc("Storage Bin", self.bin)
+
+		if self.assignment_type == "Picking":
+			storage_bin.quantity = (
+				flt(storage_bin.quantity) + flt(self.quantity)
+			)
+
+			if storage_bin.quantity <= 0:
+				storage_bin.quantity = 0
+				storage_bin.status = "Empty"
+				storage_bin.bin_assignment_record = None
+				storage_bin.assigned_item = None
+				storage_bin.uom = None
+				storage_bin.assigned_on = None
+
+			else:
+				storage_bin.status = "Occupied"
+				storage_bin.bin_assignment_record = self.name
+
+		else:
+
+			storage_bin.quantity = (
+				flt(storage_bin.quantity) + flt(self.quantity)
+			)
+
 			storage_bin.status = "Occupied"
 			storage_bin.bin_assignment_record = self.name
 			storage_bin.assigned_item = self.item
 			storage_bin.uom = self.uom
-			storage_bin.quantity = self.quantity
 			storage_bin.assigned_on = self.doa
 			storage_bin.expiry_date = self.expiry_date
-			storage_bin.save(ignore_permissions=True)
+
+		storage_bin.save(ignore_permissions=True)
 
 	def after_delete(self):
 		"""Set Storage Bin status to Empty when Bin Assignment is deleted"""
@@ -38,6 +65,7 @@ def create_bin_assignments_on_purchase_receipt_submit(doc, method):
 			# Create Bin Assignment
 			bin_assignment = frappe.get_doc({
 				"doctype": "Bin Assignment",
+				"assignment_type": "Placing",
 				"purchase_receipt": doc.name,
 				"item": item.item_code,
 				"rack": item.rack,
