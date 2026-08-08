73927e3e464bab5e7078067fdeae352f0a7f8683
tinumaria <tinumariadavis@gmail.com>
2026-08-08 14:31:41 +0530

    chore: warehouse type validation updated

--- warehouse_management/events/warehouse.py
+++ warehouse_management/events/warehouse.py
@@ -3,12 +3,12 @@ from frappe import _
 
 
 def validate_warehouse_type(doc, method=None):
-    if doc.is_sub_warehouse and doc.warehouse_type == "Bay":
+    if doc.is_sub_warehouse and doc.warehouse_type == "Storage":
         frappe.throw(
-            _("Sub warehouses cannot be marked as Bay")
+            _("Sub warehouses cannot be marked as Storage")
         )
 
-    if not doc.is_sub_warehouse and doc.warehouse_type in ["Storage", "Hold"]:
+    if not doc.is_sub_warehouse and doc.warehouse_type in ["Bay", "Hold"]:
         frappe.throw(
-            _("Main warehouses cannot be marked as Storage or Hold")
+            _("Main warehouses cannot be marked as Bay or Hold")
         )
