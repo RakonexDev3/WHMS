95524142054c2d1b97b0770cf59f8abb73be4ba0
Aravind R <aravindrajeshchry@gmail.com>
2026-08-14 14:32:36 +0530

    feat: Add user roles to login api response and restructure response

--- warehouse_management/api/login.py
+++ warehouse_management/api/login.py
@@ -2,7 +2,6 @@ import frappe
 
 @frappe.whitelist(allow_guest=True)
 def mobile_login(username, password):
-    login_manager = frappe.auth.LoginManager()
 
     try:
         login_manager = frappe.auth.LoginManager()
@@ -18,10 +17,32 @@ def mobile_login(username, password):
 
         user.save(ignore_permissions=True)
 
+        # gather user's roles and filter only custom roles
+        try:
+            user_roles = frappe.get_roles(username)
+        except Exception:
+            user_roles = []
+
+        custom_roles = (
+            frappe.get_all(
+                "Role",
+                filters={"name": ["in", user_roles], "is_custom": 1},
+                pluck="name",
+            )
+            if user_roles
+            else []
+        )
+
         return {
-            "api_key": user.api_key,
-            "api_secret": api_secret,
-            "user": username
+            "success": True,
+            "message": "Login successful",
+            "user_details": {
+                "user_id": username,
+                "user_name": user.full_name,
+                "status": "active" if user.enabled == 1 else "inactive",
+                "roles": custom_roles,
+            },
+            "user_creds": {"api_key": user.api_key, "api_secret": api_secret},
         }
 
     except Exception:
