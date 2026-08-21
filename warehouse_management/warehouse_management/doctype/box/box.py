75273cb73bab2e3f18c857f38917383a96b3b2d5
tinumaria <tinumariadavis@gmail.com>
2026-08-21 12:04:32 +0530

    feat: custom packed qty validation in pick list

--- warehouse_management/warehouse_management/doctype/box/box.py
+++ warehouse_management/warehouse_management/doctype/box/box.py
@@ -3,39 +3,24 @@
 
 import frappe
 from frappe import _
+from frappe.utils import flt
 from frappe.model.document import Document
 
 
 class Box(Document):
 	def validate(self):
-		self.validate_items()
 		self.update_total_items()
 
+	def after_insert(self):
+		self.update_pick_list_packed_qty()
+
 	def on_update(self):
+		self.update_pick_list_packed_qty()
 		self.sync_with_packing_list()
 
 	def on_trash(self):
+		self.update_pick_list_packed_qty()
 		self.remove_from_packing_list()
-
-	def validate_items(self):
-		if not self.items:
-			frappe.throw(_("At least one item is required in the Box."))
-
-		for row in self.items:
-			if not row.item:
-				frappe.throw(_("Item is required in Box."))
-
-			if not row.qty or row.qty <= 0:
-				frappe.throw(
-					_("Quantity must be greater than zero for item {0}.")
-					.format(row.item)
-				)
-
-			if not row.uom:
-				frappe.throw(
-					_("UOM is required for item {0}.")
-					.format(row.item)
-				)
 
 	def update_total_items(self):
 		"""Calculate total quantity from all Box Items."""
@@ -44,6 +29,56 @@ class Box(Document):
 			row.qty or 0
 			for row in self.items
 		)
+
+	def update_pick_list_packed_qty(self):
+		"""Update packed quantities on the linked Pick List from all boxes."""
+
+		if not self.packing_list:
+			return
+
+		pick_list_name = frappe.db.get_value(
+			"Packing List",
+			self.packing_list,
+			"pick_list",
+		)
+
+		if not pick_list_name:
+			return
+
+		boxes = frappe.get_all(
+			"Box",
+			filters={
+				"packing_list": self.packing_list,
+				"docstatus": 0,
+			},
+			pluck="name",
+		)
+
+		packed_qty = {}
+
+		if boxes:
+			box_items = frappe.get_all(
+				"Box Items",
+				filters={
+					"parent": ["in", boxes],
+					"parenttype": "Box",
+					"parentfield": "items",
+				},
+				fields=["item", "qty"],
+			)
+
+			for row in box_items:
+				packed_qty[row.item] = (
+					packed_qty.get(row.item, 0) + flt(row.qty)
+				)
+
+		pick_list = frappe.get_doc("Pick List", pick_list_name)
+
+		for row in pick_list.locations:
+			row.packed_qty = packed_qty.get(row.item_code, 0)
+
+		pick_list.flags.ignore_validate_update_after_submit = True
+		pick_list.save(ignore_permissions=True)
 
 	def sync_with_packing_list(self):
 		"""
